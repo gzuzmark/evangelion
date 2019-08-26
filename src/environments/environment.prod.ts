14b033e9dc618e8ce515df7740411a1c3c03edcd
alonso <alonso.m144@gmail.com>
2019-08-26 00:10:40 -0500

    Add environment variables files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey:'PROD_API_KEY',
+    authDomain:'PROD.firebaseapp.com',
+    databaseURL:'https://PROD.firebaseio.com',    
+    storageBucket:'PROD.appspot.com',    
+  }
 };
