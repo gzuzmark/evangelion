f845657d5ac73a58a137d6bb13ab34f08e6dccdf
gzuzmark <alonso.m144@gmail.com>
2019-08-26 00:12:52 -0500

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  firebaseConfig:{
-    apiKey:'PROD_API_KEY',
-    authDomain:'PROD.firebaseapp.com',
-    databaseURL:'https://PROD.firebaseio.com',    
-    storageBucket:'PROD.appspot.com',    
-  }
-};
